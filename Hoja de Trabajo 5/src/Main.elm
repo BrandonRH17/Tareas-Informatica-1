32041dabe8456b98f98b34aab9933e01aecd1850
unknown <rodriguez181048@unis.edu.gt>
2018-09-13 09:26:39 -0600

    hoja de trabajo 5

---
+++ Hoja de Trabajo 5/src/Main.elm
@@ -0,0 +1,45 @@
+module Main exposing(..)
+esPrimo: Int-> Bool 
+esPrimo n = esPrimoN 2 n 
+esPrimoN cont n = 
+    if n == 2 
+    then True 
+    else if modBy cont n == 0 
+    then False 
+    else if cont == n-1 
+    then True 
+    else esPrimoN (cont + 1) n
+
+fibonacci: number -> number1
+fibonacci n = 
+    if n == 0 
+    then 0
+    else if n == 1
+    then 1
+    else if n > 1
+    then fibonacci(n-1) + fibonacci(n-2)
+    else 8
+
+
+
+
+
+primos: Int -> List Int 
+primos n =
+    if n < 2 
+    then []
+    else if esPrimo n == False
+    then primos (n-1)
+    else n :: primos (n-1)
+    
+nPrimos: Int -> List Int 
+nPrimos o = aux (o , 2)
+aux (o,y) = 
+    if o == 0 
+    then []
+    else if esPrimo y == False 
+    then aux (o,y + 1)
+    else y :: aux (o-1, y+1)
+
+
+
