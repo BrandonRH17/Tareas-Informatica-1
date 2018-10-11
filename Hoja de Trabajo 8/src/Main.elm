a856d134826a96ef7fbcee54c54d33e4c1a14711
BrandonRH17 <rodriguez181048@unis.edu.gt>
2018-10-10 21:06:43 -0600

    hoja de trabajo

---
+++ Hoja de Trabajo 8/src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+zipWith : (a -> b -> c) -> List a -> List b -> List c
+zipWith n fs zs  =
+
+    case (fs, zs) of
+
+        ([], _) -> []
+
+        (_, []) -> []
+
+        (f::fss, z::zss) -> n f z ::zipWith n fs zs
+
+groupBy : (a -> Bool) -> List a -> (List a, List a) 
+groupBy m list = (siFunciona m list, noFunciona m list)
+
+siFunciona j list = case list of 
+    [] -> []
+    (b::bss) -> if j b 
+        then siFunciona j bss 
+        else b:: siFunciona j bss
+
+noFunciona j list = case list of 
+    [] -> []
+    (k::kss) -> if j k
+        then k:: noFunciona j kss 
+        else noFunciona j kss
+
+bind : Maybe a1 -> (a1 -> Maybe a) -> Maybe a
+bind h j = case h of 
+    Nothing -> Nothing 
+    Just a -> j a 
+
+
+
+
+
+
+
+
